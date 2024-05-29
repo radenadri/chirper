919c21d8592d7099939b728dc744f6279b5d400a
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-05-30 00:37:02 +0700

    feat: updating dependencies

--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -61,16 +61,20 @@ on([
     <h1
         class="mt-6 mb-6 text-2xl font-extrabold leading-none tracking-tight text-neutral-900 md:text-3xl lg:text-4 dark:text-neutral-50">
         {{ __('Latest chirps') }}</h1>
-    <div class="mt-6 bg-neutral-50 shadow-sm rounded-lg divide-y">
+    <div class="mt-6 divide-y rounded-lg shadow-sm bg-neutral-50">
         @forelse ($chirps as $chirp)
-            <div class="p-6 flex space-x-2" wire:key="{{ $chirp->id }}">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none"
-                    viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                </svg>
+            <div class="flex p-6 space-x-4" wire:key="{{ $chirp->id }}">
+                {{-- Default Avatar --}}
+                <div class="relative w-10 h-10 overflow-hidden rounded-full bg-neutral-100 dark:bg-neutral-600">
+                    <svg class="absolute w-12 h-12 text-neutral-400 -left-1" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </div>
+
                 <div class="flex-1">
-                    <div class="flex justify-between items-center">
+                    <div class="flex items-center justify-between">
                         <div>
                             <span class="text-gray-800">{{ $chirp->user->name }}</span>
                             <small
@@ -83,7 +87,7 @@ on([
                             <x-dropdown>
                                 <x-slot name="trigger">
                                     <button>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400"
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-400"
                                             viewBox="0 0 20 20" fill="currentColor">
                                             <path
                                                 d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
@@ -115,7 +119,7 @@ on([
             </div>
         @endforelse
     </div>
-    <div x-data="loadMoreChirps" x-intersect="load" class="mt-6 justify-center flex">
+    <div x-data="loadMoreChirps" x-intersect="load" class="flex justify-center mt-6">
         <div x-show="shown" x-transition>
             <x-spinner />
         </div>
