de3ffb4e162a42ad81c49fc7afaf8aebb7263dd2
Adriana Eka Prayudha <radenadriep@gmail.com>
2026-03-25 12:21:02 +0700

    feat: replace dashboard route with chirps as default redirect

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,9 +1,5 @@
 <nav class="-mx-3 flex flex-1 justify-end">
     @auth
-        <a href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-neutral-50 dark:hover:text-neutral-50/80 dark:focus-visible:ring-neutral-50">
-            Dashboard
-        </a>
         <a href="{{ route('chirps') }}"
             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-neutral-50 dark:hover:text-neutral-50/80 dark:focus-visible:ring-neutral-50">
             Chirps
